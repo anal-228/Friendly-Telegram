7b3a5d1784afdf933bd8a68489689ed64ce3262b
anal-228 <77571592+anal-228@users.noreply.github.com>
2021-01-17 16:01:42 +0300

    Upload file

---
+++ phoneseeker.py
@@ -0,0 +1,21 @@
+from .. import loader, utils
+import telethon
+import logging
+logger = logging.getLogger(__name__)
+def register(cb):
+    cb(PhoneSeakMod())
+@loader.tds
+class PhoneSeakMod(loader.Module):
+    """Ищет открытые номера в группе."""
+    strings = {"name": "PhoneSeeker"}
+    async def seekcmd(self, message):
+        await message.edit("<b>Ищем.</b>")
+        users = await message.client.get_participants(message.to_id)
+        await message.client.send_message("me", "--------------------")
+        for user in users:
+            if not user.bot:
+                if str(user.phone) != 'None':
+                    await message.client.send_message("me", f"@{user.username} - +{user.phone}")
+        await message.client.send_message("me", "--------------------")
+
+
